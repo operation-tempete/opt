28d02c3f33cc5539d4f22c1a1bc2e4279e9b3858
operation-tempete <operation-tempete@users.noreply@github.com>
2015-06-11 01:33:40 +0200

    Final app

--- OPT/app/src/main/java/com/example/lysias/opt/TestActivity.java
+++ OPT/app/src/main/java/com/example/lysias/opt/TestActivity.java
@@ -4,6 +4,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,13 +16,24 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.androidplot.Plot;
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.XYStepMode;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
@@ -34,13 +48,17 @@ public class TestActivity extends ActionBarActivity {
     private static final int NB_VALUES = 2;
     private boolean activateBuzzer = true;
     private boolean hasBeenConstructed = false;
-    ListView lv;
+    private ValuesGetter getter;
+    public int alcohol_index = 0;
+    public int sound_index = 0;
 
     class ValuesGetter implements Runnable {
         private static final int NB_VALUES = 20;
         private BluetoothSocket socket;
-        public int alcoholValue;
-        public int soundValue;
+        private static final int SAMPLE_SIZE = 20;
+        public int[] alcoholValues = new int[SAMPLE_SIZE];
+        public int[] soundValues = new int[SAMPLE_SIZE];
+        int index = 0;
 
         public ValuesGetter(BluetoothSocket socket) {
             this.socket = socket;
@@ -79,22 +97,26 @@ public class TestActivity extends ActionBarActivity {
             int alcohol = 0;
 
             try {
-                alcoholValue = Integer.parseInt(parts[0].replaceAll("\\s", ""));
-                soundValue = Integer.parseInt(parts[1].replaceAll("\\s", ""));
+                alcoholValues[index] = Integer.parseInt(parts[0].replaceAll("\\s", ""));
+                soundValues[index] = Integer.parseInt(parts[1].replaceAll("\\s", ""));
             }
             catch(Exception e) {
                 return;
             }
 
-            System.out.print("alcohol = " + alcoholValue);
-            System.out.println("; sound = " + soundValue);
+            System.out.print("alcohol = " + alcoholValues[index]);
+            System.out.println("; sound = " + soundValues[index]);
 
             runOnUiThread(new Runnable() {
                 public void run() {
                     TextView twAlcohol = (TextView) findViewById(R.id.textViewAlcohol);
-                    twAlcohol.setText(Integer.toString(alcoholValue));
+                    twAlcohol.setText(Integer.toString(alcoholValues[index]));
                     TextView twSound = (TextView) findViewById(R.id.textViewSound);
-                    twSound.setText(Integer.toString(soundValue));
+                    twSound.setText(Integer.toString(soundValues[index]));
+
+                    index++;
+                    if (index == SAMPLE_SIZE)
+                        index = 0;
                 }
             });
         }
@@ -121,7 +143,6 @@ public class TestActivity extends ActionBarActivity {
 
         if (!hasBeenConstructed) {
             BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
-            lv = (ListView) findViewById(R.id.listView);
 
             if (!BA.isEnabled()) {
                 Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -141,11 +162,12 @@ public class TestActivity extends ActionBarActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                new Thread(new ValuesGetter(this.socket)).start();
+                //new Thread(new ValuesGetter(this.socket)).start();
+                getter = new ValuesGetter(this.socket);
+                new Thread(getter).start();
             }
 
-            final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
-            lv.setAdapter(adapter);
+            createGraph(savedInstanceState);
 
             hasBeenConstructed = true;
         }
@@ -196,12 +218,209 @@ public class TestActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
-    public void updateValues() {
-        Random generator = new Random();
-        test[TEST_LENGTH - 1] = generator.nextInt(100);
-    }
-
     public void Quit(View view) {
         System.exit(0);
+    }
+
+    // redraws a plot whenever an update is received:
+    private class MyPlotUpdater implements Observer {
+        Plot plot;
+
+        public MyPlotUpdater(Plot plot) {
+            this.plot = plot;
+        }
+
+        @Override
+        public void update(Observable o, Object arg) {
+            plot.redraw();
+        }
+    }
+
+    private XYPlot dynamicPlot;
+    private MyPlotUpdater plotUpdater;
+    SampleDynamicXYDatasource data;
+    private Thread myThread;
+
+    public void createGraph(Bundle savedInstanceState) {
+
+        // android boilerplate stuff
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_display_message);
+
+        // get handles to our View defined in layout.xml:
+        dynamicPlot = (XYPlot) findViewById(R.id.dynamicXYPlot);
+
+        plotUpdater = new MyPlotUpdater(dynamicPlot);
+
+        // only display whole numbers in domain labels
+        dynamicPlot.getGraphWidget().setDomainValueFormat(new DecimalFormat("0"));
+
+        // getInstance and position datasets:
+        data = new SampleDynamicXYDatasource();
+        SampleDynamicSeries sine1Series = new SampleDynamicSeries(data, 0, "Alcohol");
+        SampleDynamicSeries sine2Series = new SampleDynamicSeries(data, 1, "Sound");
+
+        LineAndPointFormatter formatter1 = new LineAndPointFormatter(
+                Color.rgb(3, 191, 61), null, null, null);
+        formatter1.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
+        formatter1.getLinePaint().setStrokeWidth(10);
+        dynamicPlot.addSeries(sine1Series, formatter1);
+
+        LineAndPointFormatter formatter2 =
+                new LineAndPointFormatter(Color.rgb(48, 140, 232), null, null, null);
+        formatter2.getLinePaint().setStrokeWidth(10);
+        formatter2.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
+
+        //formatter2.getFillPaint().setAlpha(220);
+        dynamicPlot.addSeries(sine2Series, formatter2);
+
+        // hook up the plotUpdater to the data model:
+        data.addObserver(plotUpdater);
+
+        // thin out domain tick labels so they dont overlap each other:
+        dynamicPlot.setDomainStepMode(XYStepMode.INCREMENT_BY_VAL);
+        dynamicPlot.setDomainStepValue(5);
+
+        dynamicPlot.setRangeStepMode(XYStepMode.INCREMENT_BY_VAL);
+        dynamicPlot.setRangeStepValue(50);
+
+        dynamicPlot.setRangeValueFormat(new DecimalFormat("###.#"));
+
+        // uncomment this line to freeze the range boundaries:
+        dynamicPlot.setRangeBoundaries(0, 700, BoundaryMode.FIXED);
+
+        // create a dash effect for domain and range grid lines:
+        DashPathEffect dashFx = new DashPathEffect(
+                new float[] {PixelUtils.dpToPix(3), PixelUtils.dpToPix(3)}, 0);
+        dynamicPlot.getGraphWidget().getDomainGridLinePaint().setPathEffect(dashFx);
+        dynamicPlot.getGraphWidget().getRangeGridLinePaint().setPathEffect(dashFx);
+
+
+    }
+
+    @Override
+    public void onResume() {
+        // kick off the data generating thread:
+        myThread = new Thread(data);
+        myThread.start();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        data.stopThread();
+        super.onPause();
+    }
+
+    class SampleDynamicXYDatasource implements Runnable {
+
+        // encapsulates management of the observers watching this datasource for update events:
+        class MyObservable extends Observable {
+            @Override
+            public void notifyObservers() {
+                setChanged();
+                super.notifyObservers();
+            }
+        }
+
+        public static final int SINE1 = 0;
+        public static final int SINE2 = 1;
+        private static final int SAMPLE_SIZE = 20;
+        private MyObservable notifier;
+        private boolean keepRunning = false;
+        private int i = 0;
+
+        {
+            notifier = new MyObservable();
+        }
+
+        public void stopThread() {
+            keepRunning = false;
+        }
+
+        //@Override
+        public void run() {
+            try {
+                keepRunning = true;
+                boolean isRising = true;
+                while (keepRunning) {
+
+                    Thread.sleep(500); // decrease or remove to speed up the refresh rate.
+
+                    notifier.notifyObservers();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public int getItemCount(int series) {
+            return SAMPLE_SIZE;
+        }
+
+        public Number getX(int series, int index) {
+            if (index >= SAMPLE_SIZE) {
+                throw new IllegalArgumentException();
+            }
+            return index;
+        }
+
+        public Number getY(int series, int index) {
+            if (index >= SAMPLE_SIZE)
+                throw new IllegalArgumentException();
+
+            if (series == 0) {
+                if (alcohol_index == SAMPLE_SIZE)
+                    alcohol_index = 0;
+                return getter.alcoholValues[alcohol_index++];
+            }
+            if (series == 1) {
+                if (sound_index == SAMPLE_SIZE)
+                    sound_index = 0;
+                return getter.soundValues[sound_index++];
+            }
+            return 0;
+        }
+
+        public void addObserver(Observer observer) {
+            notifier.addObserver(observer);
+        }
+
+        public void removeObserver(Observer observer) {
+            notifier.deleteObserver(observer);
+        }
+
+    }
+
+    class SampleDynamicSeries implements XYSeries {
+        private SampleDynamicXYDatasource datasource;
+        private int seriesIndex;
+        private String title;
+
+        public SampleDynamicSeries(SampleDynamicXYDatasource datasource, int seriesIndex, String title) {
+            this.datasource = datasource;
+            this.seriesIndex = seriesIndex;
+            this.title = title;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public int size() {
+            return datasource.getItemCount(seriesIndex);
+        }
+
+        @Override
+        public Number getX(int index) {
+            return datasource.getX(seriesIndex, index);
+        }
+
+        @Override
+        public Number getY(int index) {
+            return datasource.getY(seriesIndex, index);
+        }
     }
 }
