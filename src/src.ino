cd6e7f700e90078740ca9b775846d525d36c3b20
operation-tempete <operation-tempete@users.noreply@github.com>
2015-06-11 15:38:08 +0200

    Fix buzzer

--- src/src.ino
+++ src/src.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
- SoftwareSerial bt(2, 1);
+SoftwareSerial bt(2, 1);
 #define PIN_BUZZER 0
 #define PIN_GAS   A3
 #define PIN_SOUND A2
@@ -8,52 +8,52 @@
 #define LOUD 250
 #define GAS_IT 10
 
- int BluetoothData; // the data given from Computer
- char sbuf[64];
- bool force_beep = false;
- bool 
+int BluetoothData; // the data given from Computer
+char sbuf[64];
+bool force_beep = false;
 
 int get_alcohol(void) {
-  short i = 0;
-  long sum = 0;
-  short average = 0;
-  
-  for (i = 0; i < GAS_IT; ++i)
-    sum += analogRead(PIN_GAS);
-  average = sum / GAS_IT;
-  return average;
+    short i = 0;
+    long sum = 0;
+    short average = 0;
+
+    for (i = 0; i < GAS_IT; ++i)
+        sum += analogRead(PIN_GAS);
+    average = sum / GAS_IT;
+    return average;
 }
 
- void setup() {
-   bt.begin(9600);
-   pinMode(PIN_BUZZER, OUTPUT);
-   pinMode(PIN_GAS, INPUT);
-   pinMode(PIN_SOUND, INPUT);
- }
+void setup() {
+    bt.begin(9600);
+    pinMode(PIN_BUZZER, OUTPUT);
+    pinMode(PIN_GAS, INPUT);
+    pinMode(PIN_SOUND, INPUT);
+}
 
- void loop() {
-   int gas;
-   int snd;
-   
+void loop() {
+    int gas;
+    int snd;
+
     if (bt.available()) {
-       BluetoothData=bt.read();
-       if(BluetoothData=='1') {
-          digitalWrite(PIN_BUZZER, 1);
-          force_beep = true;
-          //bt.println("LED  On D13 ON ! ");
-       }
-       if (BluetoothData=='0') {
-           digitalWrite(PIN_BUZZER ,0);
-           force_beep = false;
-           //bt.println("LED  On D13 Off ! ");
-       }
+        BluetoothData=bt.read();
+        if(BluetoothData=='1') {
+            digitalWrite(PIN_BUZZER, 1);
+            force_beep = true;
+            //bt.println("LED  On D13 ON ! ");
+        }
+        if (BluetoothData=='0') {
+            digitalWrite(PIN_BUZZER ,0);
+            force_beep = false;
+            //bt.println("LED  On D13 Off ! ");
+        }
     }
     gas = get_alcohol();
     snd = analogRead(PIN_SOUND);
     if (force_beep == false)
-      digitalWrite(PIN_BUZZER, (gas > ALCOOLISM || snd > LOUD));
+        digitalWrite(PIN_BUZZER, gas > ALCOOLISM);
+        //digitalWrite(PIN_BUZZER, (gas > ALCOOLISM || snd > LOUD));
     snprintf(sbuf, sizeof(sbuf), "%d - %d\n", gas, snd);
     bt.println(sbuf);
     delay(100);
- }
+}
 
